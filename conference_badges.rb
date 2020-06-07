a9402c82527b48118edebda5386469d0e17d605b
Jonathan Snipe <jon.mike919@gmail.com>
2020-06-07 02:48:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+
+
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+  "Hello, my name is #{name}."
+end
+  
+ 
+def batch_badge_creator(speakers)
+  batch_badge_creator(speakers).each do |badge|
+  end
+    puts badge
+  assign_rooms(speakers).each do |something|
+    puts something
+  end
+end
